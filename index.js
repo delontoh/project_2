1596c857265a130563bcf269a7ad6d83d5b745e3
delontoh <delontoh89@gmail.com>
2018-07-13 11:15:57 +0800

    push for heroku

--- index.js
+++ index.js
@@ -310,7 +310,8 @@ app.get('/calendar', getCalendar);
  * ===================================
  */
 
-const server = app.listen(3000, () => console.log('~~~ Ahoy we go from the port of 3000!!!'));
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => console.log('~~~ Ahoy we go from the port of ' + PORT + '! ~~~'));
 
 // Handles CTRL-C shutdown
 function shutDown() {
